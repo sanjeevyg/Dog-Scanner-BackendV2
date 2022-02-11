fa0836ffb2377907cd17d946d9002e0ef01e401e
sanjeevyg <yogi.sjv@gmail.com>
2022-02-10 23:42:15 -0500

    migrations schema updated

--- migrations/20220210023437_rating-two.js
+++ migrations/20220210023437_rating-two.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+exports.up = async knex => {
+    await knex.schema.createTable("emojis", (table)=> {
+          table.string("emoji")
+          table.integer("count")
+          }
+      )
+  };
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+  exports.down = async knex => {
+      await knex.schema.dropTableIfExists("emojis")
+  };
   
-};
