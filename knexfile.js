9cb655ee466028ac948fd47c79aa14c6839506fb
sanjeevyg <yogi.sjv@gmail.com>
2022-02-11 22:38:51 -0500

    knexfile.js udpated with production object

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,19 @@
   development: {
     client: 'pg',
     connection: 'postgress:///dog-scanner-database'
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      tablename: 'knex_migrations',
+      directory: './migrations', 
+    },
+    seeds: { directory: './seeds' }
   }
   
 };
